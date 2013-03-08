7506c97541b49c0fb5e50883c5bb6133fdf04a22
etu5 <ndmariane@gmail.com>
2013-03-08 13:27:39 +0100

    creation du projet

---
+++ Projet/src/main/java/dcll/nndi/projet/App.java
@@ -0,0 +1,13 @@
+package dcll.nndi.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
