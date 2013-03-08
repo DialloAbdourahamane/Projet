beaee02047fd53d8d52019af35591bb69431168c
etu5 <ndmariane@gmail.com>
2013-03-08 14:00:41 +0100

    #1 creation de la classe XMLtoJSON permettant de parser un fichier Moodle XML au format JSON

--- Projet/src/main/java/dcll/nndi/projet/App.java
+++ Projet/src/main/java/dcll/nndi/projet/App.java
@@ -1,13 +1,26 @@
 package dcll.nndi.projet;
 
 /**
- * Hello world!
- *
+ * App est la classe permettant de lancer la conversion d'un fichier au format
+ * Moodle XML au format Json et inversement.
+ * @author Ndèye Mariane NDIAYE & Mohamed Amine LOUHIDI
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+/**1.On crait une instance de la classe effectuant la conversion souhaitée
+ * 2.On fait appel à la méthode permettant d'effectuer le parse en lui
+ * donnant en paramètre le fichier à traiter 3.On exécute le code 4.On
+ * raffraichit le projet 5.Le resultat de la conversion pourra être 
+ * consulté dans le fichier créé apres execution.
+ * ConversionXMLToJSON.json pour une conversion de Moodle XML à Json
+ * @param args
+ * @throws Exception
+ */
+
+	public static void main(String[] args) throws Exception {
+		XMLtoJSON testXml = new XMLtoJSON();
+		testXml.convertXmlToJson("ExempleXml.xml");
+		System.out.println("Conversion XML To JSON : resultat disponible dans"+
+				" le fichier ConversionXMLToJSON.json");
+	}
 }
+
