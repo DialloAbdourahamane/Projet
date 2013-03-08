6c80fb53fd847b0a73f8383bc385cbcb5b43cd3d
etu5 <ndmariane@gmail.com>
2013-03-08 14:38:49 +0100

    #3 creation de la classe All tests

---
+++ Projet/src/test/java/dcll/nndi/projet/AllTests.java
@@ -0,0 +1,15 @@
+package dcll.nndi.projet;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+/**
+ * automatisation de toutes les classes de test 
+ * @author Ndèye Mariane NDIAYE & Mohamed Amine LOUHIDI
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class, XMLtoJSONTest.class })
+public class AllTests {
+
+}
